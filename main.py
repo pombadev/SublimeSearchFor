78e120637b56660bf10ecaec46704b7f0f72134e
Pomba <pomba.magar@dragonlaw.com.hk>
2018-01-09 20:23:49 +0545

    minor changes

--- main.py
+++ main.py
@@ -6,26 +6,23 @@ import sublime_plugin
 import webbrowser
 
 
-class SearchGoogleForCommand(sublime_plugin.TextCommand):
+class SublimeSearchForCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        url = 'https://www.google.com.np/search?q={}'.format(self.textSelected)
+        url = 'https://www.google.com.np/search?q={}'.format(self.get_text())
         webbrowser.open_new_tab(url=url)
 
-    # set the selected text
-    def set_text(self):
-        self.textSelected = self.view.substr(self.view.sel()[0])
+    # get the selected text
+    def get_text(self):
+        return self.view.substr(self.view.sel()[0])
 
     def is_visible(self):
-        self.set_text()
-
-        if self.textSelected:
+        if self.get_text():
             return True
         else:
             return False
 
     # adds captions to context menu usi
     def description(self):
-        self.set_text()
-
-        if self.textSelected:
-            return 'Search Google for "{}"'.format(self.textSelected)
+        text = self.get_text()
+        if text:
+            return 'Search Google for "{}"'.format(text)
